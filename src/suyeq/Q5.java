7923064a54ef6794bc6edbaf2b36b7d22aca6658
suyeq <473721601@qq.com>
2019-03-20 12:30:41 +0800

    第五题

--- src/suyeq/Q5.java
+++ src/suyeq/Q5.java
@@ -9,10 +9,58 @@ package suyeq;
  */
 public class Q5 {
     public static void main(String[] args){
-
+        long start=System.currentTimeMillis();
+        String s="zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+        System.out.println(longestPalindrome(s));
+        long end=System.currentTimeMillis();
+        System.out.println((end-start));
     }
 
+    /**
+     * s(i,j)=true
+     * s[i]==s[j]&&s[i+1]==s[j-1].......
+     * @param s
+     * @return
+     */
     public static String longestPalindrome(String s) {
-        return null;
+        int start=0;
+        int end=0;
+        if (s.length() == 0){
+            return "";
+        }
+        char t=s.charAt(0);
+        int flag=0;
+        for (int i=0;i<s.length();i++){
+            if (t != s.charAt(i)){
+                flag=1;
+            }
+        }
+        if (flag==0){
+            return s;
+        }
+        int longStringLength=0;
+        int length=s.length();
+        for (int i=0;i<length;i++){
+            for (int j=i;j<length;j++){
+                if (isNeed(s,i,j)){
+                    if (j-i >= longStringLength){
+                        longStringLength=j-i+1;
+                        start=i;
+                        end=j+1;
+                    }
+                }
+            }
+        }
+        return s.substring(start,end);
+    }
+
+    public static boolean isNeed(String s,int i,int j){
+        if (i == j || (i-1)==j){
+            return true;
+        }
+        if (s.charAt(i) == s.charAt(j)){
+            return isNeed(s,i+1,j-1);
+        }
+        return false;
     }
 }
